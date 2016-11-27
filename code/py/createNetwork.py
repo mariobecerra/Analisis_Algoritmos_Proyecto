ee4b4b330e7bc5041fed173e8e4cebcab270a56d
tmendozas <taniamendoza.s@gmail.com>
2016-11-26 19:00:54 -0600

    Adds similarity filter

--- code/py/createNetwork.py
+++ code/py/createNetwork.py
@@ -43,31 +43,34 @@ user_track_count.map(
 ##################
 #  Histograms
 ##################
-user_track_count = sc.textFile(path + "/plays-matrix.tsv").map(
-                    lambda line: [int(x) for x in line.split('\t')]
+
+path = '/Users/tania/source/itam/maestria/Analisis_Algoritmos_Proyecto/data/lastfm-dataset-1K'
+
+play_matrix = sc.textFile(path + "/plays-matrix.tsv").map(
+                    lambda line: line.split('\t')
                   )
 
-distinct_users = user_track_count.map(
-  lambda (user,track,count): user)
+users_count = play_matrix.map(
+  lambda (user,track,count): (user,1)).reduceByKey(lambda a,b: a+b).map(lambda (k,v):v)
 
-distinct_users.histogram([0, 10, 50, 100,500,990,995])
+# users_count.histogram([0,1,2,3,4,5, 10, 50, 100,500,7809])
 
-distinct_tracks = user_track_count.map(
-  lambda (user,track,count): track)
+track_counts = play_matrix.map(
+  lambda (user,track,count): (track,1)).reduceByKey(lambda a,b: a+b).map(lambda (k,v):v)
 
-distinct_tracks.histogram([0, 10, 50, 100,500,990,995])
+# track_counts.histogram([0,1,2,3,4,5, 10, 50, 100,710])
 
 #################################################
 #  Eliminar usuarios y tracks 'sin importancia'
 #################################################
 
-distinct_users = distinct_users.map(
-  lambda x: (x,1)
-).reduceByKey(lambda a,b: a+b).filter(lambda (x,c): c > 25)
-
-distinct_tracks = distinct_tracks.map(
-  lambda x: (x,1)
+filtered_users = play_matrix.map(
+  lambda (user,track,count): (user,1)
 ).reduceByKey(lambda a,b: a+b).filter(lambda (x,c): c > 10)
+
+filtered_tracks = play_matrix.map(
+  lambda (user,track,count): (track,1)
+).reduceByKey(lambda a,b: a+b).filter(lambda (x,c): c > 1)
 
 
 
@@ -80,21 +83,23 @@ user_track_count = sc.textFile(
                       path + "/plays-matrix.tsv"
                     ).map(
                       lambda line: line.split('\t')
-                    ).map(lambda (u,t,c): (int(u),(int(t),int(c))))
+                    ).map(lambda (u,t,c): (u,(t,int(c))) )
 
 filtered_user_track_count = user_track_count.join(
-    distinct_users
+    filtered_users
   ).map(
     lambda (user,((track,count),j)): (track,(user,count))
   ).join(
-    distinct_tracks
+    filtered_tracks
   ).map(
     lambda (track,((user,count),j)): ((user,track),count)
   )
 
+filtered_user_track_count.map(lambda ((user,track),count): (track,1)).reduceByKey(lambda a,b: a+b).count()
+
 
 ratings = filtered_user_track_count.map(
-              lambda ((user,track),count): Rating(user, track, float(count)))
+              lambda ((user,track),count): Rating(int(user), int(track), float(count)))
 
 # Build the recommendation model using Alternating Least Squares
 rank = 50
@@ -123,8 +128,28 @@ similarities = prod_features.cartesian(
                   lambda ((k1,v1),(k2,v2)): k1<k2
                 ).map(
                   lambda ((k1,v1),(k2,v2)): ((k1,k2),1-cosine(v1,v2))
+                ).filter(
+                  lambda ((k1,k2),sim): sim > 0
                 )
 similarities.map(lambda ((k1,k2),sim): str(k1) + '\t' + str(k2) + '\t' + str(sim) 
-                  ).saveAsTextFile(path + "/similarities-50")     
+                  ).saveAsTextFile(path + "/similarities-50")    
+
+##############################
+
+path = '/Users/tania/source/itam/maestria/Analisis_Algoritmos_Proyecto/data/lastfm-dataset-1K'
+
+similarities = sc.textFile(
+                  path + "/similarities-50.tsv"
+                ).map(
+                  lambda line: line.split('\t')
+                ).map(
+                  lambda (p1,p2,sim): ((int(p1),int(p2)),float(sim))
+                )
+
+similarities.map(lambda x: x[1]).histogram(5)
+similarities.filter(lambda x: x[1]>0.4).map(
+    lambda ((k1,k2),sim): str(k1) + '\t' + str(k2) + '\t' + str(sim)
+  ).saveAsTextFile(path+"filtered-similarities-50/")
+
 
 
